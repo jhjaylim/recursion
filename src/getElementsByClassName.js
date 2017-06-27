3723ac6cb7694152c10d4f51b95b27b9ab65a277
Jay Lim <jhjaylim@gmail.com>
2017-06-27 15:36:47 -0700

    getElements done

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,60 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+
+//document.body
+//element.childNodes
+//element.classList
+var count = 0;
+var nodeAdded = 0;
+var getElementsByClassName = function(className, node) {
+	var myCount = count;
+	
+
+	var myElementList = [];
+	if (node===undefined) {
+		node = document.body;	
+	}
+
+	if (hasClassName(node.classList, className)) {
+	
+		myElementList.push(node);
+		
+	}
+
+	if (node.childNodes.length!==0) {
+		
+		for (var i=0; i<node.childNodes.length; i++) {
+			
+			myConcat(myElementList, getElementsByClassName(className, node.childNodes[i]))
+			
+		}
+	}
+
+	return myElementList;
+ 
 };
+function hasClassName(classList, className){
+
+	var result = false;
+	if (classList&& classList.contains(className)) {
+		return true;
+	}
+	return result;
+}
+
+function myConcat(arr1,arr2) {
+	
+	
+	for (j=0; j<arr2.length;j++) {
+		arr1.push(arr2[j]);
+	}
+	
+
+}
+
+
+
+
+
+
